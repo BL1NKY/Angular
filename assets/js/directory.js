8d877a40ca53b975bd2b6b1f01b7f453b66ef049
Tobias Unger <tobias.unger1@gmx.de>
2017-01-04 01:20:04 +0100

    Until Ep.10

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -1,9 +1,22 @@
 angular.module('directoryApp', [])
-	.controller('directoryController', function ($scope) {
-		$scope.list = [
-			{name: 'Scott', age: 29},
-			{name: 'Ben', age: 32},
-			{name: 'Ross', age: 31},
-			{name: 'Courtney', age: 29}
-		]
-	});
+  .controller('directoryController', function () {
+
+    var dirList = this;
+
+    dirList.toggle = false;
+
+    dirList.list = [
+      {name: 'Scott', age: 29},
+      {name: 'Ben', age: 32},
+      {name: 'Ross', age: 31},
+      {name: 'Courtney', age: 29}
+    ];
+
+    dirList.addPerson = function () {
+      if (dirList.name != "" && dirList.age != "") {
+        dirList.list.push({name: dirList.name, age: dirList.age});
+      }
+      dirList.name = "";
+      dirList.age = "";
+    };
+  });
